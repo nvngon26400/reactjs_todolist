e6637205ee0a7b57af25b40f9a977e234f522475
nvngon2604 <nvngon2604@gmail.com>
2021-10-04 09:54:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './App.css';
 import Header from "./components/Header";
 import Form from "./components/Form";
 import TodosList from "./components/TodosList";
 
 const App = () => {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="container">
       <div className="app-wrapper">
@@ -19,10 +26,11 @@ const App = () => {
             setInput={setInput}
             todos={todos}
             setTodos={setTodos}
+            setEditTodo={setEditTodo}
           />
         </div>
         <div>
-          <TodosList todos={todos} setTodos={setTodos} />
+          <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
         </div>
       </div>
     </div>
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,6 +1,26 @@
 ﻿import React from "react";
 
-const TodosList = ({todos, setTodos}) => {
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+  const handleComplete = (todo) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleEdit = ({ id }) => {
+    const findTodo = todos.find((todo) => todo.id === id);
+    setEditTodo(findTodo);
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div>
       {todos.map((todo) => (
@@ -8,18 +28,27 @@ const TodosList = ({todos, setTodos}) => {
           <input
             type="text"
             value={todo.title}
-            className="list"
+            className={`list ${todo.completed ? "complete" : ""}`}
             onChange={(event) => event.preventDefault()}
           />
           <div>
-            <button className="button-complete task-button">
-                <i className="fa fa-check-circle"></i>
+            <button
+              className="button-complete task-button"
+              onClick={() => handleComplete(todo)}
+            >
+              <i className="fa fa-check-circle"></i>
             </button>
-            <button className="button-edit task-button">
-                <i className="fa fa-edit"></i>
+            <button
+              className="button-edit task-button"
+              onClick={() => handleEdit(todo)}
+            >
+              <i className="fa fa-edit"></i>
             </button>
-            <button className="button-delete task-button">
-                <i className="fa fa-trash"></i>
+            <button
+              className="button-delete task-button"
+              onClick={() => handleDelete(todo)}
+            >
+              <i className="fa fa-trash"></i>
             </button>
           </div>
         </li>
